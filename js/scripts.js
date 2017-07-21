d27efd8e83d878ec077d94cbf4ca170cda7cec21
Kate Byars <katebyars5@gmail.com>
2017-07-21 11:18:02 -0700

    display pizzas in the order summary area

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,45 @@
 $(document).ready(function(){
 //BACK END LOGIC
-// function Pizza (nameOf, size, price) {
-//   this.nameOfPizza = nameOf;
-//   this.size = size;
-//   this.price = price;
-//   this.toppings = [];
-// }
-//
-// Pizza.prototype.cost = function calculateCost(size, toppingsArray) {
-//   var cost = 0;
-// if (size = "large") {
-//     cost +=5;
-//   } else if (size = "medium") {
-//     cost +=4;
-//   } else if (size = "small") {
-//     cost +=3;
-//   }
-//   for (i=0; i < toppingsArray.length; i +=1) {
-//     cost +=i;
-//   }
-//   return cost;
+function Pizza (nameOf, sizeOf, price) {
+  this.nameOfPizza = nameOf;
+  this.sizeOf = size;
+  this.price = price;
+  this.toppings = [];
+}
+
+Pizza.prototype.cost = function calculateCost(size, toppingsArray) {
+  var cost = 0;
+if (size = "large") {
+    cost +=5;
+  } else if (size = "medium") {
+    cost +=4;
+  } else if (size = "small") {
+    cost +=3;
+  }
+  for (i=0; i < toppingsArray.length; i +=1) {
+    cost +=i;
+  }
+  return cost;
 }
 //FRONT END LOGIC
 
 $("#addPizza").click(function(event){
   event.preventDefault();
-    alert("hi");
-    var currentName = $("#pizzaName").val();
-    alert("his");
-    alert(currentName);
-  // $("#pizzaDisplay").text(currentName);
-  // var currentSize = $("#size").val();
-  // var currentToppings = $("input:checkbox[name=toppings]:checked").each(function(){
-  //   $(this).val();
-  // });
-  // var currentPrice = 0;
-  // var currentPizza = new Pizza (currentName, currentSize, currentPrice);
-  // currentPrice = currentPizza.cost(currentSize, currentToppings);
-  //
-  $("#pizzaDisplay").text(currentName);
+  var currentName = $("#pizzaName").val();
+  var currentSize = $("#size").val();
+  var currentPrice = 0;
+  var currentToppings = [];
+  var listOfToppings = [];
+  $("input:checkbox[name=toppings]:checked").each(function(){
+    var currentToppings= $(this).val();
+    listOfToppings.push(currentToppings);
+  });
+ var currentPizza = new Pizza (currentName, currentSize, currentPrice);
+  currentPrice = currentPizza.cost(currentSize, currentToppings);
+  $("#pizzaDisplay").append("<li>" + "$" + currentPrice + " "+ currentName + "'s " + currentSize + " pizza has: " + listOfToppings + "<br>");
 });
-//
-// $("#submitOrder").submit(function(){
-//   hide order details + show 'your order is on the way'
-// });
+
+$("#submitOrder").click(function(){
+  // hide order details + show 'your order is on the way'
+});
 });
