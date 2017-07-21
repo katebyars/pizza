b05396dd4a18a333720490952889045df9000cad
Kate Byars <katebyars5@gmail.com>
2017-07-21 09:43:13 -0700

    new changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,32 @@
 $(document).ready(function(){
 //BACK END LOGIC
-function Pizza (nameOf, size, price) {
-  this.nameOfPizza = nameOf;
-  this.size = size;
-  this.price = price;
-  this.toppings = [];
-}
-
-Pizza.prototype.cost = function calculateCost(size, toppingsArray) {
-  var cost = 0;
-if (size = "large") {
-    cost +=5;
-  } else if (size = "medium") {
-    cost +=4;
-  } else if (size = "small") {
-    cost +=3;
-  }
-  for (i=0; i < toppingsArray.length; i +=1) {
-    cost +=i;
-  }
-  return cost;
-}
+// function Pizza (nameOf, size, price) {
+//   this.nameOfPizza = nameOf;
+//   this.size = size;
+//   this.price = price;
+//   this.toppings = [];
+// }
+//
+// Pizza.prototype.cost = function calculateCost(size, toppingsArray) {
+//   var cost = 0;
+// if (size = "large") {
+//     cost +=5;
+//   } else if (size = "medium") {
+//     cost +=4;
+//   } else if (size = "small") {
+//     cost +=3;
+//   }
+//   for (i=0; i < toppingsArray.length; i +=1) {
+//     cost +=i;
+//   }
+//   return cost;
+// }
 //FRONT END LOGIC
 
-$("#addPizza").click(function(){
-  alert("hi");
-  var currentName = $("#pizzaName").val();
+$("#addPizza").submit(function(event){
+  event.preventDefault();
+  debugger;
+  var currentName = $("input#pizzaName").val();
   var currentSize = $("#size").val();
   var currentToppings = $("input:checkbox[name=toppings]:checked").each(function(){
     $(this).val();
@@ -33,7 +34,8 @@ $("#addPizza").click(function(){
   var currentPrice = 0;
   var currentPizza = new Pizza (currentName, currentSize, currentPrice);
   currentPrice = currentPizza.cost(currentSize, currentToppings);
-  $("#pizzaDisplay").text(this.Pizza);
+
+  // $("#pizzaDisplay").text(this.Pizza);
 });
 //
 // $("#submitOrder").submit(function(){
