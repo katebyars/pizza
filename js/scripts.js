88c57605ef640066fd268bee5458b3aa12437259
Kate Byars <katebyars5@gmail.com>
2017-07-25 16:46:01 -0700

    restructure

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 //BACK END LOGIC
-  function Pizza (nameOf, sizeOf, priceOf) {
-    this.nameOfPizza = nameOf;
+  function Pizza(nameOf, sizeOf, priceOf){
+    this.nameOf = nameOf;
     this.sizeOf = sizeOf;
     this.priceOf = 0;
     this.toppings = [];
   }
-  Pizza.prototype.cost = function calculateCost(size, toppingsArray) {
+  Pizza.prototype.getToppings(){
+
+  }
+  Pizza.prototype.cost(size, toppingsArray) {
     var cost = 0;
     if (size === "Large") {
       cost +=5;
@@ -19,28 +22,23 @@
     }
     return cost;
   }
+Pizza.prototype.display(price, naname, size, toppings){
+  $("#pizzaDisplay").append("<li>" + "$" + this.price + " "+ this.nameOfPizza + "'s " + this.sizeOf + " pizza has: " + this.toppings + "<br>");
+    listOfToppings = [];
+}
 
-  // var currentName = $("#pizzaName").val();
-  // var currentSize = $("#size").val();
-  // var currentToppings = [];
-  // var listOfToppings = [];
 
-  // var currentPizza = new Pizza (currentName, currentSize, currentPrice);
-  // this.price = currentPizza.cost(currentSize, listOfToppings);
-  // this.nameOfPizza = currentName;
-  // this.sizeOf = currentSize;
-  // this.toppings = listOfToppings;
+//FRONT END LOGIC
 $(document).ready(function(){
-  //FRONT END LOGIC
-  $("#addPizza").click(function(event){
-    event.preventDefault();
+  event.preventDefault();
+
+  $("#addPizza").submit(function(){
     $(".deliveryGuy").hide();
+    var newPizza = new Pizza("input#pizzaName", "input#size", price );
     $("input:checkbox[name=toppings]:checked").each(function(){
-      var currentToppings= $(this).val();
-      listOfToppings.push(currentToppings);
+      newPizza.toppings.push();
     });
-    $("#pizzaDisplay").append("<li>" + "$" + this.price + " "+ this.nameOfPizza + "'s " + this.sizeOf + " pizza has: " + this.toppings + "<br>");
-      listOfToppings = [];
+    newPizza.display();
   });
   $("#submitOrder").click(function(){
     var address = $("#submitOrderAddress").val();
